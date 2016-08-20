2ab8aafe91b6dbb7dd33ff76ee29d273529f1025
Jeremy M. Reed <reeje76@gmail.com>
2016-08-20 12:59:45 -0600

    Implement isAnagram()

--- anagram.js
+++ anagram.js
@@ -1,11 +1,14 @@
+'use strict'
+
 var validator = require('validator'),
   NotImplementedException = require('./exceptions/NotImplementedException.js'),
-  InvalidParameterException = require('./exceptions/InvalidParameterException.js');
+  InvalidParameterException = require('./exceptions/InvalidParameterException.js'),
+  InvalidStateException = require('./exceptions/InvalidStateException.js');
 
 const ALPHABET = 'abcdefghijklmnopqrstuvwxyz ';
 
 var Anagram = function(word) {
-  this.word = word;
+  this.refWord = this.sanitize(word);
 };
 
 /*
@@ -16,7 +19,39 @@ Anagram.prototype.matches = function() {
 };
 
 Anagram.prototype.isAnagram = function(testWord) {
-  throw new NotImplementedException();
+  var refDict = {},
+    i = 0;
+
+  if (testWord.length !== this.refWord.length) {
+    return false;
+  }
+
+  // Build reference dictionary.
+  for (i = 0 ; i < this.refWord.length ; i++ ) {
+    if (refDict[this.refWord[i]] === undefined) {
+      refDict[this.refWord[i]] = 1;
+    } else {
+      refDict[this.refWord[i]] = refDict[this.refWord[i]] + 1;
+    }
+  }
+
+  // Check testWord against refDict.
+  for (i = 0 ; i < testWord.length ; i++ ) {
+    var letter = testWord[i];
+    if (refDict[letter] === undefined) {
+      return false;
+    } else if (refDict[letter] === 1) {
+      delete refDict[letter];
+    } else if (refDict[letter] > 1 ) {
+      refDict[letter] -= 1;
+    } else {
+      // Huh?
+      throw new InvalidStateException('Encountered some sort of invalid state');
+    }
+  }
+
+  // Final test.
+  return Object.keys(refDict).length === 0;
 };
 
 Anagram.prototype.sanitize = function(rawWord) {
